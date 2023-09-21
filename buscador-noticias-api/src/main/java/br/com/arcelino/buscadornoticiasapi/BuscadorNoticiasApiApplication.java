f6931bfd2497cc526bae8d666fa1c41db8c2e347
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2023-09-21 18:39:35 -0300

    Projeto Base Backend

---
+++ buscador-noticias-api/src/main/java/br/com/arcelino/buscadornoticiasapi/BuscadorNoticiasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.arcelino.buscadornoticiasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscadorNoticiasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscadorNoticiasApiApplication.class, args);
+	}
+
+}
---
+++ buscador-noticias-api/src/test/java/br/com/arcelino/buscadornoticiasapi/BuscadorNoticiasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arcelino.buscadornoticiasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscadorNoticiasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
